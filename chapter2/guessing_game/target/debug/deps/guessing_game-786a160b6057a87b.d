66da7ebfc5068bb7ef87faf8c3601ee58c652e72
Christopher Chalcraft <chris.chalcraft@gmail.com>
2020-06-06 00:37:00 -0600

    Adding the guessing game

---
+++ chapter2/guessing_game/target/debug/deps/guessing_game-786a160b6057a87b.d
@@ -0,0 +1,5 @@
+/home/cchalc/Tutorials/rustbook/chapter2/guessing_game/target/debug/deps/guessing_game-786a160b6057a87b: src/main.rs
+
+/home/cchalc/Tutorials/rustbook/chapter2/guessing_game/target/debug/deps/guessing_game-786a160b6057a87b.d: src/main.rs
+
+src/main.rs:
---
+++ chapter2/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/cchalc/Tutorials/rustbook/chapter2/guessing_game/target/debug/guessing_game: /home/cchalc/Tutorials/rustbook/chapter2/guessing_game/src/main.rs
